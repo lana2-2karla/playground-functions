38a06c7be9c239aaa8b14550f3a12ee045f27c88
Allana Karla <allanakarla1702@gmail.com>
2021-11-19 11:51:30 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-} //VQV
-
+function techList(arrList, name) {
+  
+  arrList.sort();
+  if (arrList.length === 0) {
+    return "Vazio!";
+  }
+   let arrObject = [];
+  for (let i = 0; i < arrList.length; i += 1) {
+   arrObject.push({ tech: arrList[i], name});
+  }
+ return arrObject;
+};
+  
+  
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
